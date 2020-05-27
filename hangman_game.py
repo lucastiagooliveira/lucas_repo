60de9da8fbf2170e71ceb15a5e4fc4c9f7f8ac42
lucastiagooliveira <62961381+lucastiagooliveira@users.noreply.github.com>
2020-05-26 21:37:08 -0300

    Add files via upload

--- hangman_game.py
+++ hangman_game.py
@@ -2,70 +2,70 @@ print(':::::::::::::::: HANGMAN GAME ::::::::::::::::::::::')
 
 status = ['''
 
-	=========
-	|       |
-		    |
-		    |
-		    |
-		    |
-		 ======''',
+    =========
+    |       |
+            |
+            |
+            |
+            |
+         ======''',
 '''
 
-	=========
-	|       |
-	O	    |
-		    |
-		    |
-		    |
-		 ======''',
+    =========
+    |       |
+    O	    |
+            |
+            |
+            |
+         ======''',
 '''
 
-	=========
-	|       |
-	O	    |
-	|	    |
-		    |
-		    |
-		 ======''',
+    =========
+    |       |
+    O	    |
+    |	    |
+            |
+            |
+         ======''',
 
 '''
 
-	=========
-	|       |
-	O	    |
+    =========
+    |       |
+    O	    |
    /|	    |
-		    |
-		    |
-		 ======''',
+            |
+            |
+         ======''',
 '''
 
-	=========
-	|       |
-	O	    |
+    =========
+    |       |
+    O	    |
    /|\	    |
-		    |
-		    |
-		 ======''',
+            |
+            |
+         ======''',
 
 '''
 
-	=========
-	|       |
-	O	    |
+    =========
+    |       |
+    O	    |
    /|\	    |
    / 	    |
-		    |
-		 ======''',
+            |
+         ======''',
 
 '''
 
-	=========
-	|       |
-	O	    |
+    =========
+    |       |
+    O	    |
    /|\	    |
    / \ 	    |
-		    |
-		 ======'''
+            |
+         ======'''
 
 ]
 
@@ -73,12 +73,30 @@ open_file = open('C:/Users/lucas/Documents/lucastiagooliveira/game.txt','r')
 words = open_file.read().split()
 open_file.close()
 
-from random import choice 
+from random import choice
 word = choice(words)
-underlines = ('_ ' for i in range(len(word)))
-print(''.join(underlines))
+underlines = list('_ ' for i in range(len(word)))
+underlines = ''.join(underlines)
+chances = 0
+print(word)
+word = list(x for x in word)
+word1 = list(0 for i in range(0, len(word)))
+while chances < 5:
+    print(underlines)
+    p = str(input('Type a letter to complete the word: '))
+    cont = 0
+    for i in list(x for x in word):
+        if i == p:
+            underlines = list(i for i in underlines)
+            underlines[cont] = p
+            underlines = ''.join(underlines)
+            word1[int(cont/2)] = p
+        cont += 2
+    if word1 == word:
+        print('Congratulation! \n You got the word: ', ''.join(word))
+        break
+    chances += 1
 
-# w = input('Type one letter for complete the word:')
 
 
 
