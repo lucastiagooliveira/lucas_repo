b9454a0ea5886fe8a1a68e181f052e4943c65519
Lucas Tiago <lucastiagooliveira@gmail.com>
2020-07-23 11:20:33 -0300

    finish

--- Study Materials/Stanford University - Machine Learning/machine-learning-ex5/ex5/learningCurve.m
+++ Study Materials/Stanford University - Machine Learning/machine-learning-ex5/ex5/learningCurve.m
@@ -53,6 +53,16 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+  X_train = X(1:i,:);
+  y_train = y(1:i);
+  
+  theta = trainLinearReg(X_train, y_train, lambda); %Set the theta
+  
+  error_train(i) = linearRegCostFunction(X_train, y_train, theta, 0);
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+ 
+endfor
 
 
 
--- Study Materials/Stanford University - Machine Learning/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ Study Materials/Stanford University - Machine Learning/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -11,21 +11,31 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
-
+alpha = 0.01;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
 %               regression for a particular choice of theta.
 %
 %               You should set J to the cost and grad to the gradient.
 %
+h = X*theta;
+
+reg = (lambda/m)*theta;
+
+reg_1 = reg.*ones(size(reg));
+reg_1(1,:) = 0;
+
+grad = ((1/m) * X' * (h - y)) + reg_1;
+
+%theta  = theta - alpha * grad;
+
+reg_cost = (lambda/(2*m)) .* theta.^2;
+reg_cost_1 = ones(size(reg_cost)).*reg_cost;
+reg_cost_1(1) = 0;
+reg_cost_1 = sum(reg_cost_1);
 
 
-
-
-
-
-
-
+J = (1/(2*m))*(sum(sum((h - y).^2))) + reg_cost_1;
 
 
 
